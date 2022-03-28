01765275a927baba18073cb2c2d93ef5afd370ee
最初のコミットです。 <>
2022-03-29 00:06:38 +0900

    contactReducerの「;」を省いた

--- src/reducers/contactReducer.js
+++ src/reducers/contactReducer.js
@@ -6,16 +6,12 @@ export const initialState = {
 export const contactReducer = (state, action) => {
   switch(action.type){
     case 'sending':
-      return {sendMessage: '問い合わせ送信中です。', sendStatus: 1};
-      break;
+      return {sendMessage: '問い合わせ送信中です。', sendStatus: 1}
     case 'done':
-      return {sendMessage: '問い合わせ送信完了しました。', sendStatus: 2};
-      break;
+      return {sendMessage: '問い合わせ送信完了しました。', sendStatus: 2}
     case 'finish':
-      return {...state, sendStatus: 0};
-      break;
+      return {...state, sendStatus: 0}
     default:
-      return {...state};
-      break;
+      return {throw new Error();}
   }
 }
